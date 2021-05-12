84f5d57802f90e4c6c1f5704bcf37aaa88999de0
sayad <realPaulSayad@gmail.com>
2021-05-12 18:28:53 -0400

    Alarm objects are now initialized from the EditAlarmScreensViewController and to a dictionary which are used in Defaults.swift

--- Alarm Clock App/Alarm.swift
+++ Alarm Clock App/Alarm.swift
@@ -13,4 +13,20 @@ struct Alarm {
     var alarmPeriod : String
     var alarmDays : [Bool]
     var alarmToggle : Bool
+    
+    init(dictionary : NSDictionary) {
+        alarmName = dictionary["name"] as! String
+        alarmTime = dictionary["time"] as! String
+        alarmPeriod = dictionary["period"] as! String
+        alarmDays = dictionary["days"] as! [Bool]
+        alarmToggle = dictionary["toggle"] as! Bool
+    }
+    
+    init(alarmName: String, alarmTime: String, alarmPeriod: String, alarmDays: [Bool], alarmToggle: Bool) {
+        self.alarmName = alarmName
+        self.alarmTime = alarmTime
+        self.alarmPeriod = alarmPeriod
+        self.alarmDays = alarmDays
+        self.alarmToggle = alarmToggle
+    }
 }
